d1fe7891533905194ad6e2970565dc3ec8c8c40f
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-02-03 11:29:55 -0300

    test: adicionando testes da rota de deposito

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -2,37 +2,60 @@ import { AppError } from "../../../../shared/errors/AppError";
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
 import { AuthenticateUserUseCase } from "../../../users/useCases/authenticateUser/AuthenticateUserUseCase";
 import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
 let usersRepositoryInMemory: InMemoryUsersRepository;
+let iStatementsRepository: InMemoryStatementsRepository;
+
 let authenticateUserUseCase: AuthenticateUserUseCase;
 let createUserUseCase: CreateUserUseCase;
+let createStatementUseCase: CreateStatementUseCase;
 
-describe("Rota de login", () => {
+describe("Rota de deposit", () => {
   beforeAll(() => {
     usersRepositoryInMemory = new InMemoryUsersRepository();
+    iStatementsRepository = new InMemoryStatementsRepository();
     authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
     createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    createStatementUseCase = new CreateStatementUseCase(usersRepositoryInMemory,iStatementsRepository);
   })
 
-  it("Deve ser possível fazer login:", async () => {
+  it("Deve ser possível fazer um deposito: ", async () => {
     const mockUser = {
-      name: "Ana",
-      email: "ana.clara@email.com",
-      password: "1234",
-    }
+      name: "teste",
+      email: "emailTeste",
+      password: "123",
+    };
 
     await createUserUseCase.execute(mockUser);
 
-    const userData = await authenticateUserUseCase.execute({email: "ana.clara@email.com", password: "1234"});
+    const user = await usersRepositoryInMemory.findByEmail("emailTeste");
 
-    expect(userData).toHaveProperty("token");
+    const teste = {user_id: user?.id, type: "Deposit", amount: 100, description: "descrição"};
+
+    await createStatementUseCase.execute(teste as any);
   });
 
-  it("Não deve ser possível fazer login em uma conta não existente: ", async () => {
-    await expect(authenticateUserUseCase.execute({email: "user.nor@exist.com", password: "1234"})).rejects.toBeInstanceOf(AppError);
+  it("Não deve ser possível fazer um saque sem user_id válido: ", async () => {
+    const teste = {user_id: "userIdInvalido", type: "withdraw", amount: 10, description: "descrição"};
+
+    await expect(createStatementUseCase.execute(teste as any)).rejects.toBeInstanceOf(AppError);
   });
 
-  it("Não deve ser possível fazer login com uma senha inválida: ", async () => {
-    await expect(authenticateUserUseCase.execute({email: "ana.clara@email.com", password: "senhaInvalida"})).rejects.toBeInstanceOf(AppError);
+  it("Não deve ser possível fazer um saque sem dinheiro: ", async () => {
+    const mockUser = {
+      name: "Ana",
+      email: "ana32@gmail.com",
+      password: "123",
+    };
+
+    await createUserUseCase.execute(mockUser);
+
+    const user = await usersRepositoryInMemory.findByEmail("ana32@gmail.com");
+
+    const teste = {user_id: user?.id, type: "withdraw", amount: 10000000000, description: "descrição"};
+
+    await expect(createStatementUseCase.execute(teste as any)).rejects.toBeInstanceOf(AppError);
   });
 })
